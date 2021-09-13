26d7ee9727a2d5f5b45984de0e113d3f88672750
tiagorv <tiago.valerio@betha.com.br>
2021-09-12 21:29:32 -0300

    Adicionado o Lombok ao Projeto e adicionado geração automatica a Entidade de Cliente

--- src/main/java/aplicacao/model/entity/Cliente.java
+++ src/main/java/aplicacao/model/entity/Cliente.java
@@ -1,9 +1,12 @@
 package aplicacao.model.entity;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
+@Data
 public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
