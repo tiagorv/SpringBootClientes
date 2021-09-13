3db37418066ee135a829f49924dd700d1c75cfe3
tiagorv <tiago.valerio@betha.com.br>
2021-09-12 21:18:49 -0300

    Adicionado a @Entity de Cliente

---
+++ src/main/java/aplicacao/model/entity/Cliente.java
@@ -0,0 +1,23 @@
+package aplicacao.model.entity;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 150)
+    private String nome;
+
+    @Column(nullable = false, length = 11)
+    private String cpf;
+
+    @Column
+    private LocalDate dataCadastro;
+
+    @Column(nullable = false, length = 60)
+    private String cidade;
+}
