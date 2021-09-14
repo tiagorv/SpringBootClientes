5975593d5ad5429f1022117c58356118fa0fe864
tiagorv <tiago.valerio@betha.com.br>
2021-09-13 22:51:23 -0300

    Adicionado Controller de Clientes com a Operação de Salvar

--- src/main/java/aplicacao/rest/ClienteController.java
+++ src/main/java/aplicacao/rest/ClienteController.java
@@ -5,11 +5,11 @@ import aplicacao.model.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/clientes")
 public class ClienteController {
-
     @Autowired
     private ClienteRepository repository;
 
@@ -17,5 +17,12 @@ public class ClienteController {
     @ResponseStatus(HttpStatus.CREATED)
     public Cliente salvar(@RequestBody Cliente cliente){
         return repository.save(cliente);
+    }
+
+    @GetMapping("{id}")
+    public Cliente acharPorId( @PathVariable Integer id){
+        return repository.
+                findById(id).
+                orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 }
