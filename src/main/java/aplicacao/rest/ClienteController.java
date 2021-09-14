c1ff8471df3d867c1db14d0767977442800e2a6d
tiagorv <tiago.valerio@betha.com.br>
2021-09-13 23:00:18 -0300

    Adicionado delete a Controller de Clientes

--- src/main/java/aplicacao/rest/ClienteController.java
+++ src/main/java/aplicacao/rest/ClienteController.java
@@ -25,4 +25,15 @@ public class ClienteController {
                 findById(id).
                 orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletar(@PathVariable Integer id){
+        repository
+                .findById(id)
+                .map(cliente -> {
+                    repository.delete(cliente);
+                    return Void.TYPE;
+                    }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
 }
