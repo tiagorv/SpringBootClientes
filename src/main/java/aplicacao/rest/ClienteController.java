0b43ff891a4e986265344a0fd089cf0ab88190fb
tiagorv <tiago.valerio@betha.com.br>
2021-09-14 21:50:08 -0300

    Intercionalização de Erros

--- src/main/java/aplicacao/rest/ClienteController.java
+++ src/main/java/aplicacao/rest/ClienteController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/api/clientes")
 public class ClienteController {
@@ -15,7 +17,7 @@ public class ClienteController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Cliente salvar(@RequestBody Cliente cliente){
+    public Cliente salvar(@RequestBody @Valid Cliente cliente){
         return repository.save(cliente);
     }
 
@@ -39,7 +41,7 @@ public class ClienteController {
 
     @PutMapping("{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void atualizar(@PathVariable Integer id, @RequestBody Cliente clienteAtualizado ){
+    public void atualizar(@PathVariable Integer id, @RequestBody @Valid Cliente clienteAtualizado ){
         repository.findById(id).map(cliente -> {
             cliente.setNome(clienteAtualizado.getNome());
             cliente.setCpf(clienteAtualizado.getCpf());
